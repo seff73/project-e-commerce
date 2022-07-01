2a5b12b8708b5560f858352ab7d09ed377188829
seff73 <seff73@gmail.com>
2022-07-01 02:52:24 -0400

    Product component added

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ products, bannerData }) {
       </div>
 
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
       <FooterBanner />
